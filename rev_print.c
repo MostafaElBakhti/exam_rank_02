35ad98daacd94cf0fea0ab2f6a5ebdebd9d17f2d
Mostafa El Bakhti <mel-bakh@c2r11p7.1337.ma>
2026-02-07 18:54:40 +0100

    search and replace

--- rev_print.c
+++ rev_print.c
@@ -6,7 +6,32 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 16:46:11 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/07 16:46:14 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/07 17:33:30 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc , char *argv[])
+{	
+	int i ; 
+	int len ;
+	if (argc != 2)
+	{
+		printf("\n");
+		return 0 ; 
+	}
+	len = 0 ;
+	while(argv[1][len])
+		len++ ;
+	
+	i = 0 ; 
+	while(len > 0 )
+	{
+		printf("%c" , argv[1][len - 1]) ;
+		len--;
+	}
+    printf("\n");
+    return 0;
+}
---
+++ search_and_replace.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 17:34:41 by mel-bakh          #+#    #+#             */
+/*   Updated: 2026/02/07 18:52:06 by mel-bakh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc , char *argv[])
+{
+	if (argc != 4)
+	{
+		write(1,"\n",1);
+		exit(0);
+	}
+	int i = 0 ; 
+	int isAvailable = 0 ; 
+	while (argv[1][i])
+	{
+		if ( argv[1][i] == argv[2][0])
+		{
+			isAvailable = 1 ; 
+		}
+		i++;
+	}
+	if (isAvailable == 0)
+	{
+		int k = 0 ; 
+		while(argv[1][k]){
+			write(1,&argv[1][k], 1) ; 
+			k++ ;
+		}
+		exit(0);
+	}
+	i = 0 ; 
+	while (argv[1][i])
+	{
+		if ( argv[1][i] == argv[2][0])
+		{
+			argv[1][i] = argv[3][0] ; 
+		}
+		write(1,&argv[1][i], 1) ; 
+		i++;
+	}
+	
+}

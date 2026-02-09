19f3fb1f66cb8a643e99006f5e7143a7c392675c
mostafa <mustmustapha67@gmail.com>
2026-02-09 17:18:28 +0100

    improve code

--- level_2/do_op/do_op.c
+++ level_2/do_op/do_op.c
@@ -14,11 +14,11 @@
 
 int main (int argc , char *argv[]) 
 {
-    // if (argc != 3)
-	// {
-	// 	write(1,"\n",1);
-	// 	return(0);
-	// }
+    if (argc != 3)
+	{
+		write(1,"\n",1);
+		return(0);
+	}
 
     int i = 0 ; 
     int num1 = 0 ;
@@ -39,12 +39,7 @@ int main (int argc , char *argv[])
     int sign1 = 1 ;
     while (argv[1][i])
     {
-        if (argv[3][i] == '-')
-        {
-            sign1 = -1 ; 
-            i++;
-        }
-        if (argv[1][i] >= '0' && argv[1][i] <= '9')
+    if ((argv[1][i] >= '0' && argv[1][i] <= '9' ) || (i == 0 && argv[1][i] == '-'))
         {
             isNum1 = 1 ;
         }else{
@@ -57,15 +52,10 @@ int main (int argc , char *argv[])
     int sign2 = 1 ; 
     while (argv[3][i])
     {
-        if (argv[3][i] == '-')
-        {
-            sign2 = -1 ; 
-            i++;
-        }
-        if (argv[3][i] >= '0' && argv[3][i] <= '9')
+    if ((argv[3][i] >= '0' && argv[3][i] <= '9') || (i == 0 && argv[3][i] == '-'))
         {
             isNum2 = 1 ;
-        }else{
+        }else {
             isNum2 = 0 ; 
             break ;
         }
@@ -74,8 +64,8 @@ int main (int argc , char *argv[])
     printf("isNum1: %d, isNum2: %d\n", isNum1, isNum2);
     if (isNum1 && isNum2)
     {
-        num1 = atoi(argv[1]) * sign1;
-        num2 = atoi(argv[3]) * sign2;
+        num1 = atoi(argv[1]) ;
+        num2 = atoi(argv[3]) ;
     }else{
         write(1, "noo\n", 4);
         return (0);

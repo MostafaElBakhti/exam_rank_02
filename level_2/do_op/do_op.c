2bc0f8f9e5e99c5c8b99bbb5400bb3a4a6168f03
mostafa <mustmustapha67@gmail.com>
2026-02-09 16:52:16 +0100

    improve code

--- level_2/do_op/do_op.c
+++ level_2/do_op/do_op.c
@@ -31,19 +31,19 @@ int main (int argc , char *argv[])
     {
         len++;
     }
-    printf("%d\n\n" , len) ; 
-    // if (len != 1)
-    // {
-    //     write(1, "hoo\n", 4);
-    //     return (0);
-    // }
     if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%') || len != 1)
     {
         write(1, "hoo\n", 4);
         return (0);
     }
+    int sign1 = 1 ;
     while (argv[1][i])
     {
+        if (argv[3][i] == '-')
+        {
+            sign1 = -1 ; 
+            i++;
+        }
         if (argv[1][i] >= '0' && argv[1][i] <= '9')
         {
             isNum1 = 1 ;
@@ -54,8 +54,14 @@ int main (int argc , char *argv[])
         i++;
     }
     i = 0 ; 
+    int sign2 = 1 ; 
     while (argv[3][i])
     {
+        if (argv[3][i] == '-')
+        {
+            sign2 = -1 ; 
+            i++;
+        }
         if (argv[3][i] >= '0' && argv[3][i] <= '9')
         {
             isNum2 = 1 ;
@@ -65,17 +71,41 @@ int main (int argc , char *argv[])
         }
         i++;
     }
+    printf("isNum1: %d, isNum2: %d\n", isNum1, isNum2);
     if (isNum1 && isNum2)
     {
-        num1 = atoi(argv[1]) ;
-        num2 = atoi(argv[3]) ;
+        num1 = atoi(argv[1]) * sign1;
+        num2 = atoi(argv[3]) * sign2;
     }else{
         write(1, "noo\n", 4);
         return (0);
     }
 
-    printf("%d\n",num1) ;
-    printf("%d\n",num2) ;
+    if (argv[2][0] == '+')
+        printf("%d", num1 + num2) ;
+    else if (argv[2][0] == '-')
+        printf("%d", num1 - num2) ;
+    else if (argv[2][0] == '*')
+        printf("%d", num1 * num2) ;
+    else if (argv[2][0] == '/')
+    {
+        if (num2 == 0)
+        {
+            write(1, "noo\n", 4);
+            return (0);
+        }
+        printf("%d", num1 / num2) ;
+    }
+    else if (argv[2][0] == '%')
+    {
+        if (num2 == 0)
+        {
+            write(1, "\n", 1);
+            return (0);
+        }
+        printf("%d", num1 % num2) ;
+    }
+    write(1, "\n", 1);
     return (0) ;
 
 }

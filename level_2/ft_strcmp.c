40e9918a4d21b45cabe7de2920b2748398275d81
mostafa <mustmustapha67@gmail.com>
2026-02-10 00:26:46 +0100

    improve code

--- level_2/ft_strcmp.c
+++ level_2/ft_strcmp.c
@@ -17,15 +17,14 @@
 int    ft_strcmp(char *s1, char *s2)
 {
     int i = 0 ; 
-    int res = 0 ;
 
-    while (s1[i] && s2[i])
+
+    while (s1[i] == s2[i])
     {
-        res = s1[i] - s2[i] ; 
-        if( res != 0)
-            return res ; 
+        if (s1[i] == '\0')
+            return (0);
         i++;
     }
 
-    return 0 ; 
+    return s1[i] - s2[i] ; 
 }

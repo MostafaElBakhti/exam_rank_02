5d1af844b6f51777116379654b257f544a2051ab
mostafa <mustmustapha67@gmail.com>
2026-02-09 01:09:21 +0100

    improve code

--- repeat_alpha.c
+++ repeat_alpha.c
@@ -26,21 +26,21 @@ int main (int argc , char *argv[])
     int count ; 
     while(argv[1][i])
     {
-        while( (argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
+        if ( (argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
         {
             if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
                 count = argv[1][i] - 'a' + 1 ; 
             else 
                 count = argv[1][i] - 'A' + 1 ; 
-            break ;
-        }
-
         while(count > 0)
         {
             printf("%c" , argv[1][i]) ; 
             count-- ; 
         }
-        
+        }else{
+            printf("%c" ,argv[1][i] ) ; 
+        }
+
         count = 0 ; 
         i++;
     }

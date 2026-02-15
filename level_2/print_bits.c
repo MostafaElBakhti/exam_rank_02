b683c7b2abb696b4cf9118c011df1a48e3cf63c5
mostafa <mustmustapha67@gmail.com>
2026-02-15 20:24:43 +0000

    improve code

--- level_2/print_bits.c
+++ level_2/print_bits.c
@@ -16,7 +16,8 @@ void	print_bits(unsigned char octet)
 {
     int i = 7 ; 
     char bit ; 
-    while(i < 0)
+
+    while(i >= 0)
     {
         bit = ((octet >> i) & 1) + '0' ; 
         write(1,&bit,1) ; 

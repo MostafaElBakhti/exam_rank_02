3f1a13ccd3d4841640d9f8efcba40159a2e45df5
Mostafa El Bakhti <mel-bakh@c2r11p5.1337.ma>
2026-02-16 00:35:36 +0000

    te

--- level_2/reverse_bits.c
+++ level_2/reverse_bits.c
@@ -6,30 +6,30 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 13:53:18 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/15 13:53:18 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/16 00:12:29 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 unsigned char	reverse_bits(unsigned char octet)
 {
-    unsigned char result = 0 ; 
-    unsigned char bit ; 
 
-    int i = 8 ; 
-
-    while (i--)
-    {
-        //      0000 0000          0000 0010
-        result = (result << 1 ) | (octet & 1 ) ; 
-        octet >>= 1 ; 
-    }
-    
-
+    unsigned char tmp = 0 ; 
+	int i = 8 ; 
+	while (i--)
+	{
+		tmp = (tmp << 1) | (octet & 1 ) ; 
+		octet >>= 1 ;   
+	}
+	
+	return tmp ;
 }
 
 int main()
 {
-    reverse_bits(2) ;  
+    printf("%c" , reverse_bits(2)) ; 
 }
+// 0000 0000
+// 0000 0010

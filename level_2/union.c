70e81d4800589c3521204355c52dd67dc9abbe85
Mostafa El Bakhti <mel-bakh@c2r4p6.1337.ma>
2026-02-17 00:37:52 +0000

    update

--- level_2/union.c
+++ level_2/union.c
@@ -6,7 +6,7 @@
 /*   By: mel-bakh <mel-bakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 00:15:04 by mel-bakh          #+#    #+#             */
-/*   Updated: 2026/02/16 00:32:55 by mel-bakh         ###   ########.fr       */
+/*   Updated: 2026/02/17 00:37:33 by mel-bakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 
 int has_seen(char c)
 {
-	static char arr[256] = {0} ;
-	int i = 0 ;
-	while (arr[i])
-	{
-		if (arr[i] == c)
-			return 1 ; 
-		i++;
-	}
-	arr[i] = c ;
-	return 0 ;
+	static int arr[256] = {0} ;
+
+	if (arr[c] == 1)
+		return 1 ; 
+	arr[c] = 1 ; 
+	return 0 ; 
 }
 
 int main(int argc , char *argv[]) 
@@ -47,9 +43,9 @@ int main(int argc , char *argv[])
 	i = 0 ; 
 	while (argv[2][i])
 	{
-		if(!(has_seen(argv[1][i])))
+		if(!(has_seen(argv[2][i])))
 		{
-			printf("%c" , argv[1][i]) ; 
+			printf("%c" , argv[2][i]) ; 
 		}
 		i++ ;
 	}

663e1f85599c2769859fee21f97d5620da2d73dc
mostafa <mustmustapha67@gmail.com>
2026-03-16 16:32:26 +0000

    improve code

--- level_3/itoa.c
+++ level_3/itoa.c
@@ -14,41 +14,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	*ft_itoa(int nbr)
-{
-    int len = 0 ; 
-    int test = nbr;
-
-    if (nbr == 0)
-        len = 1;
-    while (test > 0)
-    {
-        test = test / 10 ; 
-        len++ ; 
-    }
-
-    char *str = malloc(sizeof(char) * len + 1);
-    int i = len - 1;
-
-    if (nbr == 0)
-        str[0] = '0';
-
-    int tmp = 0 ;
-    while (nbr > 0)
-    {
-        tmp = nbr % 10;
-        str[i] = tmp + '0';
-        nbr = nbr / 10;
-        i--;
-    }
-    str[len] = '\0';
-    return str ;
-}
-
-int main()
-{
-    char *str = ft_itoa(4569872);
-    write(1, str, 7);
-    free(str);
-    return (0);
-}
+char	*ft_itoa(int nbr);
